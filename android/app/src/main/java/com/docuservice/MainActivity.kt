fa68642912705af1aa4e06c02ce8b4598c565d56
michaelamoabengvaulitions <michael.amoabeng2015@gmail.com>
2025-10-26 06:23:18 +0200

    renamed app

--- android/app/src/main/java/com/docuservice/MainActivity.kt
+++ android/app/src/main/java/com/docuservice/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.anonymous.docuservice
+package com.docuservice
 import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
--- android/app/src/main/java/com/docuservice/MainApplication.kt
+++ android/app/src/main/java/com/docuservice/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.anonymous.docuservice
+package com.docuservice
 import com.facebook.react.common.assets.ReactFontManager
 
 import android.app.Application
